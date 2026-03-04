7dd53354c366e80c6643fe0fd5ab7384e192f4fa
Colin Marc <hi@colinmarc.com>
2026-03-04 15:11:13 +0100

    feat(cli): bauplan init
    Creates a new project and (crucially) generates a uuid for it.

---
+++ tests/cli/init.rs
@@ -0,0 +1,38 @@
+use std::process::Command;
+
+use anyhow::{Result, bail};
+use bstr::ByteSlice as _;
+
+#[test]
+fn init_generates_valid_dag() -> Result<()> {
+    let dir = tempfile::tempdir()?;
+
+    crate::bauplan()
+        .args(["init", "--name", "test_project"])
+        .arg(dir.path())
+        .assert()
+        .success();
+
+    let models = dir.path().join("models.py");
+
+    let output = Command::new("uv")
+        .args(["run", "ruff", "check", "--isolated"])
+        .arg(&models)
+        .output()?;
+    if !output.status.success() {
+        bail!(
+            "ruff check failed:\n{}",
+            [&output.stdout[..], &output.stderr[..]]
+                .concat()
+                .to_str_lossy()
+        );
+    }
+
+    crate::bauplan()
+        .args(["run", "--dry-run", "--cache", "off", "--strict", "on", "-p"])
+        .arg(dir.path())
+        .assert()
+        .success();
+
+    Ok(())
+}
