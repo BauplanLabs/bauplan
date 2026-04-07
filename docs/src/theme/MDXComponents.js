0743982a47df2ff3333ea3cc22cac221b65d0df1
innazh <innazh1133@gmail.com>
2026-04-07 16:03:05 -0400

    docs: add cli ref page generation script
    the script parses the cli and generates
    a cli reference page in mdx format,
    update existinglinks to the cli.

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -2,11 +2,13 @@ import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
 import * as Admonitions from "./components/Admonitions";
 import * as PyReference from "./components/PyReference";
+import * as CliReference from "./components/CliReference";
 import * as Badges from "./components/Badges";
 
 export default {
   ...MDXComponents,
   ...Admonitions,
   ...PyReference,
+  ...CliReference,
   ...Badges,
 };
