35f8cb981f8e924793461dc29b71c405bb293b27
Aldrin M <aldrin.montana@bauplanlabs.com>
2026-09-22 13:28:07 -0700

    feat: add `ColumnLineage`
    Add `ColumnLineage` as a new `FieldType` so that it can be provided as
    the `lineage` argument of `TableField`.
    
    Also updates some docstrings to clarify the `name` parameter and its
    usage for `lineage` using `TableSchema.__class_getitem__` and
    `ColumnLineage`.

--- bauplan-sdk-types/src/bauplan_sdk_types/_table_fields.py
+++ bauplan-sdk-types/src/bauplan_sdk_types/_table_fields.py
@@ -1,5 +1,6 @@
 """Bauplan pySDK stubs for typing table fields (columns)."""
 
+from dataclasses import dataclass
 from typing import Optional, TypeVar, Generic
 
 
@@ -98,6 +99,20 @@ class Any(FieldType):
     ...
 
 
+@dataclass
+class ColumnLineage(FieldType):
+    """
+    Reference to an upstream table column to inherit data and metadata from.
+
+    The reference has two required parameters:
+    1. `table_name` expects a fully qualified table name including namespace.
+    2. `col_name` expects the column name as stored in the lakehouse catalog.
+    """
+
+    table_name: str
+    col_name: str
+
+
 class TableField:
     """
     A schema field that contains metadata and is used to annotate a table column.
@@ -112,7 +127,7 @@ class TableField:
     class SampleSchema(bauplan_sdk_types.TableSchema):
         col_a: Annotated[
             bauplan_sdk_types.Any,
-            bauplan_sdk_types.TableField()
+            bauplan_sdk_types.TableField(name='.col a.')
         ]
     ```
 
@@ -122,13 +137,16 @@ class TableField:
     metadata and constraints.
 
     The attribute name, `col_a`, corresponds to the name of the represented table column.
-    If the table column's name cannot be used as the attribute name, then the `name`
-    parameter can be specified. For example, if the actual column name is stored with a
-    space: `name='col a'`.
+    If the table column's name cannot be used as the attribute name then the `name`
+    parameter can be specified, for example: `name='.col a.'`.
 
-    Lineage references another `TableField` by attribute name (python identifier) and not
-    by column name. This means that `col_a` would be referenced as
-    `SampleSchema['col_a']` and not as `SampleSchema['col a']`.
+    Lineage references another `TableField` to "inherit" or "receive" data and metadata
+    from. This is an explicit property that represents dataflow. Lineage can be specified
+    in one of two ways:
+    1. directly reference an attribute (the python identifier) in a `TableSchema`:
+    `SampleSchema['col_a']`.
+    2. define a `ColumnLineage` object using the table identifier and column name:
+    `ColumnLineage('namespace.table', '.col a.')`.
     """
 
     # TODO: make all parameters keyword args only with `*` arg marker
